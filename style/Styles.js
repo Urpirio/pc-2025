25029fcd5b13ff1425c24797af6911d10bc6e511
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:53:40 -0400

    Se añadio un con objecto con estilos estandar para diferentes apartado del Proyecto

---
+++ style/Styles.js
@@ -0,0 +1,11 @@
+
+export const StyleGlobal = {
+    StyleDropDown: `border border-gray-300 [&_ul]:flex [&_ul]:flex-col [&_ul]:gap-1 p-1 [&_li]:p-2 [&_li]:border-gray-50 [&_li]:transition-all 
+    [&_li]:duration-300 [&_li]:hover:border [&_li]:hover:border-gray-300 [&_li]:hover:bg-gray-50 [&_li]:hover:text-blue-900 [&_li]:rounded-md 
+    rounded-md bg-white shadow-2xs`,
+    StyleBodyDropDown:`border border-gray-300 rounded-md p-2 outline-0 text-gray-500 w-full shadow-2xs`,
+    StyleMaskDialogs: `backdrop-blur-xs bg-blue-600`,
+    StyleContentDialogs: {scrollbarWidth:'none'},
+    StyleInput: "border border-gray-300 rounded-md p-2 outline-0 text-gray-500 w-full shadow-2xs",
+    StyleBuscadores:"border border-gray-300 p-2 rounded-md outline-0 text-gray-500 w-70 shadow-2xs",
+}
