86116eeadad2bd66e1e7e60080bf3f794bac9f21
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:49:22 -0400

    Se mejoraron los estilos de la tabla

--- components/components - Panel-Informativo/subcomponents/Cards/Card - CasosOficina.jsx
+++ components/components - Panel-Informativo/subcomponents/Cards/Card - CasosOficina.jsx
@@ -6,24 +6,15 @@ import { DataCardCasosOficina } from '../../Data/Data - CardCasosOficina';
 
 export default function CardCasosOficina() {
   return (
-    <DataTable  value={DataCardCasosOficina}  className="
-        w-full
-        text-sm
-        [&_th]:text-gray-500
-        [&_th]:p-3
-        [&_td]:p-3
-        [&_td]:border-gray-300
-        [&_td]:font-semibold
-        [&_td]:text-gray-500
-        [&_td]:border-t
-
-       
-        ">
+    <DataTable  value={DataCardCasosOficina}  
+      className={"border w-full border-gray-300 overflow-hidden rounded-xl"}
+      tableClassName={" [&_thead>tr>th]:text-gray-600 [&_thead>tr>th]:p-2 [&_thead]:bg-white" }
+      rowClassName={' p-5 [&_td]:p-3 [&_td]:border-t [&_td]:border-gray-300'}>
         <Column  field="Oficina" header="Oficina"></Column>
-        <Column field="Reclamos" header="Reclamos"></Column>
-        <Column field="Mediaciones" header="Mediaciones"></Column>
-        <Column field="Denuncias" header="Denuncias"></Column>
-        <Column field="Total" header="Total"></Column>
+        <Column field="Reclamos" header="Reclamos" bodyClassName={'font-bold text-gray-700'}></Column>
+        <Column field="Mediaciones" header="Mediaciones" bodyClassName={'font-bold text-gray-700 '}></Column>
+        <Column field="Denuncias" header="Denuncias" bodyClassName={'font-bold text-gray-700'}></Column>
+        <Column field="Total" header="Total" bodyClassName={'font-bold text-gray-700'}></Column>
     </DataTable>
   )
 }
