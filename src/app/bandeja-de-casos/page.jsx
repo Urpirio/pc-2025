927bea5ef9fea4972e07ab87e9f4f432187d44d8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:51:45 -0400

    Se agrego la navegacion de la tabla

--- src/app/bandeja-de-casos/page.jsx
+++ src/app/bandeja-de-casos/page.jsx
@@ -2,7 +2,7 @@
 import ModalFiltros from "../../../components/components - Bandeja-casos/components/Modal - Filtros";
 import SectionBandejaCasos from "../../../components/components - Bandeja-casos/components/Section - BandejaCasos";
 import SectionTablaCasos from "../../../components/components - Bandeja-casos/components/Section - TablaCasos";
-// import SectionTablaNavegacion from "../../../components/components - Globales/components/Section - TablaNavegacion"; 
+import SectionTablaNavegacion from "../../../components/components - Globales/components/Section - TablaNavegacion"; 
 import Header from "../../../components/components - Globales/components/Header";
 
 export default function page() {
@@ -12,7 +12,7 @@ export default function page() {
         <main className="flex flex-col gap-2">
             <SectionBandejaCasos/>
             <SectionTablaCasos/>
-            {/* <SectionTablaNavegacion/> */}
+            <SectionTablaNavegacion/>
         </main>
         <ModalFiltros/>
     </body>
