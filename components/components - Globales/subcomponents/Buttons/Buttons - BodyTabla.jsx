e0e9f9a1e5721d568f11a53d8b2c4e6a7f892a4a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:45:12 -0400

    Se agrego el Menu de more

--- components/components - Globales/subcomponents/Buttons/Buttons - BodyTabla.jsx
+++ components/components - Globales/subcomponents/Buttons/Buttons - BodyTabla.jsx
@@ -4,6 +4,12 @@ import { PiWarningCircle } from "react-icons/pi";
 import Link from "next/link";
 import { LuPencilLine } from "react-icons/lu";
 import { useState } from "react";
+import { TieredMenu } from "primereact/tieredmenu";
+import { Button } from "primereact/button";
+import { useRef } from "react";
+
+
+
 
 export default function ButtonsBodyTabla() {
 
@@ -19,12 +25,27 @@ export default function ButtonsBodyTabla() {
   };
   
   const Actualizacion = ({contenido,ID}) => {
+
+    const Menu = useRef(null)
+
+    const Items = [
+      {
+        label: 'Ver detalles',
+        command: ()=>{
+          window.location.href = ID;
+        }
+      },
+    ]
+
+
+
     return (
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between '>
             <span>{contenido}</span>
-            <Link href={{pathname:`${ID}`}} >
+            <TieredMenu model={Items} popup ref={Menu} className="border p-1 bg-white border-gray-300 shadow-2xs rounded-md [&_li]:rounded-md [&_li]:border-0 [&_li]:p-2 [&_li]:hover:bg-gray-100 [&_li]:transition-all [&_li]:duration-300 [&_li]:hover:text-blue-600" breakpoint="750px" />
+            <Button onClick={(e)=>Menu.current.toggle(e)}>
                 <IoIosMore className="text-2xl"/>
-            </Link>
+            </Button>
         </div>
       )
   }
