15a87be8d3e47c34a7187d7c32b3c14420c406e5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:43:21 -0400

    Se estandarizaron los estilos

--- components/components - Colaboradores/components/Section - Colaboradores.jsx
+++ components/components - Colaboradores/components/Section - Colaboradores.jsx
@@ -3,6 +3,7 @@ import { InputText } from "primereact/inputtext";
 import { GoPlus } from "react-icons/go";
 import { useState } from "react";
 import { SetDeployPopUp_POST_PUT_Colaboradores } from "../subcomponents/Pop-Up/Pop-Up - POST-PUT_Colaboradores";
+import { StyleGlobal } from "../../../style/Styles";
 
 export default function SectionColaboradores({DeployPopUpColaboradores}) {
 
@@ -19,7 +20,7 @@ export default function SectionColaboradores({DeployPopUpColaboradores}) {
             <h1 className="text-2xl text-blue-900 font-semibold">Colaboradores</h1>
         </div>
         <div className="flex justify-between items-center">
-            <InputText placeholder="Buscar Colaboradores..." className="border border-gray-300 p-2 rounded-md outline-0 text-gray-500 w-70 shadow-2xs" 
+            <InputText placeholder="Buscar Colaboradores..." className={StyleGlobal.StyleBuscadores}
                 value={Textvalue} onChange={(e)=>{setTextvalue(e.target.value)}} />
             <button onClick={BtnAgregar} className="flex items-center bg-blue-900 px-3 py-2 rounded-md text-white hover:bg-blue-900/80 transition-all duration-300 cursor-pointer">
                 <GoPlus/>
