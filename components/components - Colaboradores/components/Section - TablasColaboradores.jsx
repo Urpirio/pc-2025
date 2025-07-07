2ed09cd9dc47724f566a7518fabb80ff01e48c3f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:43:32 -0400

    Se mejoro el estilo de la tabla

--- components/components - Colaboradores/components/Section - TablasColaboradores.jsx
+++ components/components - Colaboradores/components/Section - TablasColaboradores.jsx
@@ -23,25 +23,11 @@ export default function SectionTablasColaboradores({DeployPopUpColaboradores}) {
 
   return (
    <section className="px-5">
-    <DataTable className='
-   w-full
-   rounded-md
-   text-sm
-   [&_th]:text-gray-500
-   [&_th>div]:flex
-   [&_th>div]:items-center
-   [&_th>div]:gap-2
-   [&_th]:p-3
-   [&_td]:p-3
-   [&_td]:border-gray-300
-   [&_td]:text-gray-900
-   [&_td]:border-t
-   [&_td]:py-3
-   [&_td>div]:flex
-   [&_td>div]:items-center
-   [&_td>div]:gap-2
-
-  ' value={DataTablaColaboradores} >
+    <DataTable
+      className={"border w-full border-gray-300  overflow-hidden rounded-xl"}
+      tableClassName={" [&_thead>tr>th>div]:flex [&_thead>tr>th>div]:gap-5 [&_thead>tr>th]:text-gray-500 [&_thead>tr>th]:p-2 [&_thead]:bg-white" }
+      rowClassName={' p-5 [&_td]:p-3 [&_td]:border-t [&_td]:border-gray-300'} 
+      value={DataTablaColaboradores} >
         <Column field="NumeroEmpleado" header={'No. de empleado'} />
         <Column field="Nombre" sortable header={'Nombre'} />
         <Column field="Correo" sortable header={'Correo'} />
