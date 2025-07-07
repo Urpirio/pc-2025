3519f7b71a4d613fa0fac0f080c28f8db72d61fb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-07 15:45:30 -0400

    Se estandarizo los estilo

--- components/components - Oficinas/components/Section - Oficinas.jsx
+++ components/components - Oficinas/components/Section - Oficinas.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { AutoComplete } from 'primereact/autocomplete';
 import { SetDeployPopUp_POST_PUT_Oficinas } from "../subcomponents/Pop-Up/Pop-Up - POST-PUT_Oficinas";
 import { DataDropDown } from "../../components - Globales/Data/Data - DropDown";
+import { StyleGlobal } from "../../../style/Styles";
 
 
 export default function SectionOficinas({DeployPopUpOficinas}) {
@@ -33,7 +34,7 @@ export default function SectionOficinas({DeployPopUpOficinas}) {
         <div className="flex justify-between items-center">
             {/* <AutoComplete value={SearchValue} placeholder="Buscar oficina..." onChange={(e)=>setSearchValue(e.target.value)} suggestions={TextSearch} completeMethod={Search}  
                 className="border [&_input]:border-0 [&_input]:outline-none [&_input]:w-full border-gray-300 p-2 rounded-md outline-0 text-gray-500 w-70 shadow-2xs" /> */}
-            <InputText placeholder="Buscar Oficinas..." className="border border-gray-300 p-2 rounded-md outline-0 text-gray-500 w-70 shadow-2xs" 
+            <InputText placeholder="Buscar Oficinas..." className={StyleGlobal.StyleBuscadores}
                 value={TextSearch} onChange={(e)=>{setTextSearch(e.target.value)}} />
             <button onClick={BtnAgregarOficina} className="flex items-center bg-blue-900 px-3 py-2 rounded-md text-white hover:bg-blue-900/80 transition-all duration-300 cursor-pointer">
                 <GoPlus/>
